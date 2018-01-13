e69b7c4ae8e63f9b6672713392c33eec58a14336
cagataycali <cagataycali@icloud.com>
2018-01-13 08:01:58 +0300

    🎉  Init

---
+++ demo.js
@@ -0,0 +1,3 @@
+const gifi = require('./index')
+
+gifi('https://gifi.cagatay.me/1.pdf').then(filename => console.log(filename))
---
+++ index.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+module.exports = url => {
+  return new Promise(async (resolve, reject) => {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: false
+    })
+    const page = await browser.newPage()
+    await page.goto(`https://gifi.cagatay.me/?pdf=${url}`)
+
+    page.on('console', async msg => {
+      var data = msg._text.replace(/^data:image\/\w+;base64,/, '')
+      var buffer = Buffer.from(data, 'base64')
+      let d = new Date()
+      let filename = `${d.valueOf()}.gif`
+      fs.writeFile(filename, buffer)
+      await browser.close()
+      resolve(filename)
+    })
+  })
+}
